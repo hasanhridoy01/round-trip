8abea194026e81c01232589d30c2d9b172572c85
hasanhridoy01 <hasanhridoy1367@gmail.com>
2025-08-08 18:59:50 +0600

    fied

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'export',
+  output: "export", // <-- uncomment this if you're doing static hosting on Netlify
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true, // required for static export when using <Image>
+  },
 };
 
 module.exports = nextConfig;
